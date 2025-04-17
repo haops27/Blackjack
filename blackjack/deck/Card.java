b4e398f7b77e5de7a4028d4fdf85b97e9b83fcf9
diennguyenhuy <diennguyenhuy6005@gmail.com>
2025-04-17 18:27:39 +0700

    Create Card.java

---
+++ blackjack/deck/Card.java
@@ -0,0 +1,37 @@
+package blackjack.deck;
+
+public class Card {
+	// Name of each cards in folder "cards"
+	private String value; // Số
+	private String type;  // Chất
+	
+	Card(String value, String type){
+		this.value = value;
+		this.type = type;
+	}
+	
+	public String toString() {
+		return value + " - " + type;
+	}
+	
+	//get value of each cards: 2-10 are equal to themselves; J, Q, K are considered as 10; Ace is 1 or 11 for some case
+	public int getValue() {
+		if ("JQK".contains(value)) {
+			return 10;
+		}
+		if (this.isAce()) {
+			return 11;
+		}
+		return Integer.parseInt(value);
+	}
+	
+	//check that a cards is an Ace or not
+	public boolean isAce() {
+		return value.equals("A");
+	}
+	
+	// getting image of each cards
+	public String getImagePath() {
+		return "./cards/" + toString() + ".png";
+	}
+}
